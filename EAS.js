9a16c67ee0eff6650d1ec0e171b0cb5a0ba1d2a8
Josh Bolipata <joshtiffton@gmail.com>
2022-02-10 20:13:31 +0800

    clear

--- EAS.js
+++ EAS.js
@@ -5,3 +5,16 @@ for (i = 0; i < 16; i++) {
         container.appendChild(div);
     } 
 }
+
+const alldivs = document.querySelectorAll('.container div');
+alldivs.forEach((div) => {
+    div.addEventListener(('mouseenter'), () => {
+        div.setAttribute("class", "hovered");
+    })
+})
+
+const clearbutton = document.querySelector('.clear');
+clearbutton.addEventListener(('click'), () => {
+    let blackdivs = document.querySelectorAll('.hovered');
+    blackdivs.forEach((div))
+})
